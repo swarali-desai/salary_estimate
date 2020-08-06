5a98f9d34b5002355a81767c2979a39490c93edb
swarali <swaralidesai75@gmail.com>
2020-08-06 13:51:42 +0530

    data cleaning notebook created

--- data_collection.py
+++ data_collection.py
@@ -2,8 +2,16 @@ import glassdoor_webscrapper as gs
 import pandas as pd 
 
 path = "C:/Program Files (x86)/chromedriver.exe"
+'''
+# fetched data for following roles:
+1. Data scientist : 150
+2. Data analyst : 75
+3. Machine learning engineer : 75
+4. Deep learning engineer : 75
+5. Business Analyst: 75
+6. Big Data Engineer: 75
+'''
+df = gs.get_jobs('NLP Engineer',75, False, path, 15)
 
-df = gs.get_jobs('data scientist',1000, False, path, 15)
-# print(df.head())
 
-df.to_csv('glassdoor_jobs.csv', index = False)
+df.to_csv('glassdoor_jobs_NLP.csv', index = False)
